44a73c8e6b25611b41cf335b2a9593cd576a229c
Yuvraj <ys2029472@gmail.com>
2024-01-23 12:38:15 +0530

    update comment

--- cypress/e2e/AutomationProject/tabel.js
+++ cypress/e2e/AutomationProject/tabel.js
@@ -1,6 +1,6 @@
 import { Given, When } from "cypress-cucumber-preprocessor/steps";
 import { formfill } from "../../PageObjectModel/formFill";
-
+/// git 
 const formfillPage = new formfill
 
 When('user click on tabel',()=>{
